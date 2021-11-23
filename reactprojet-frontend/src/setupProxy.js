509d87d0cc56c5a45ae3c9aa05d3186e4b45b3cf
papimasome <papimasome@gmail.com>
2021-11-23 18:54:20 +0330

    Add project files.

---
+++ reactprojet-frontend/src/setupProxy.js
@@ -0,0 +1,17 @@
+﻿const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/weatherforecast",
+    "/api",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'https://localhost:7254',
+        secure: false,
+    });
+
+    app.use(appProxy);
+
+ 
+};
